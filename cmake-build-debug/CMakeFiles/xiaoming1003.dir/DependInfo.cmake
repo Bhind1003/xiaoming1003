f135a00c2d095831285fa49fda5a94037b86b76d
陈明瀚 <58127395+Bhind1003@users.noreply.github.com>
2021-03-18 17:08:33 +0800

    2021年3月18日 14:22:42 pat5-1

---
+++ cmake-build-debug/CMakeFiles/xiaoming1003.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/xyz/xiaoming1003/ctest/time.cpp" "E:/xyz/xiaoming1003/cmake-build-debug/CMakeFiles/xiaoming1003.dir/ctest/time.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../ctest"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/xiaoming1003.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/xiaoming1003.dir/ctest/time.cpp.obj"
+  "libxiaoming1003.dll.a"
+  "xiaoming1003.exe"
+  "xiaoming1003.exe.manifest"
+  "xiaoming1003.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/xiaoming1003.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
