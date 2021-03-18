67d21a1c0c797f658ea02e2ab5ab39e8302b4b80
陈明瀚 <58127395+Bhind1003@users.noreply.github.com>
2021-03-18 14:22:55 +0800

    2021年3月18日 14:22:42 pat5-1

--- ctest/time.cpp
+++ ctest/time.cpp
@@ -1,48 +1,42 @@
 #include <bits/stdc++.h>
 
-#define maxn 113
+#define max 113
 using namespace std;
-int dp[maxn][maxn], path[maxn][maxn], a[maxn][maxn];
+struct node {
+    char name[14];
+    int n;
+};
+bool cmp(struct node){
 
+}
 int main() {
-    int n, m;
-    cin >> n >> m;
-    for (int i = 1; i <= n; i++)
-        for (int j = 1; j <= m; j++)
-            cin >> a[i][j];
-    for (int j = 1; j <= m; j++)//第一行
-    {
-        dp[1][j] = dp[1][j - 1] + a[1][j];
-        path[1][j] = 1;
+    int n;
+    cin >> n;
+    char name[n][14];
+    for (int i = 0; i < n; ++i) {
+        cin >> name[i];
     }
-    for (int i = 1; i <= n; i++)//第一列
-    {
-        dp[i][1] = dp[i - 1][1] + a[i][1];
-        path[i][1] = 1;
+    int tn;
+    cin >> tn;
+    struct node arr[tn];
+    double sum = 0.0;
+    for (int i = 0; i < tn; ++i) {
+        cin >> arr[i].name >> arr[i].n;
+        sum += arr[i].n;
     }
-    for (int i = 2; i <= n; i++) {
-        for (int j = 2; j <= m; j++) {
-            if (dp[i - 1][j] < dp[i][j - 1])//上<左
-            {
-                dp[i][j] = dp[i][j - 1] + a[i][j];
-                path[i][j] = path[i][j - 1];
-            } else if (dp[i - 1][j] > dp[i][j - 1]) {
-                dp[i][j] = dp[i - 1][j] + a[i][j];
-                path[i][j] = path[i - 1][j];
-            } else//两者值相同
-            {
-                dp[i][j] = dp[i - 1][j] + a[i][j];
-                path[i][j] = path[i - 1][j] + path[i][j - 1];
-            }
-        }
-    }
-    cout << dp[n][m] << " " << path[n][m];
+    sum /= tn;
+    cout << "Bing Mei You" << endl;
     return 0;
 }
 /*
-4  5
-2  -1  6  -2  9
--3  2  5  -5  1
-5   8  3  -2  4
-5   2  8  -4  7
+10 GAO3 Magi Zha1 Sen1 Quan FaMK LSum Eins FatM LLao
+8
+Magi 50
+Pota 30
+LLao 3
+Ammy 48
+Dave 15
+GAO3 31
+Zoro 1
+Cath 60
 */
