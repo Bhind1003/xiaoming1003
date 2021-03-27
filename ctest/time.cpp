fe4703cf23764b42896b9eb0d361b9970fe2ae25
陈明瀚 <58127395+Bhind1003@users.noreply.github.com>
2021-03-27 16:47:12 +0800

    pat train1-5 get 17points

--- ctest/time.cpp
+++ ctest/time.cpp
@@ -2,42 +2,49 @@
 
 using namespace std;
 
-int main() {
-    int n, i, k, count = 0;
-    cin >> n;
-    unordered_map<string, int> hash;
-    vector<string> out;
-    for (i = 0; i < n; i++) {
-        string c1, c2;
-        cin >> c1 >> c2;
-        hash[c1]++;
-        hash[c2]++;
-        k = hash[c1] > hash[c2] ? hash[c1] : hash[c2];
+bool fun(int n) {
+    if (n <= 2) {
+        return true;
     }
-    unordered_map<string, int>::iterator iter;
-    iter = hash.begin();
-    while (iter != hash.end()) {
-        if (k == iter->second) {
-            out.push_back(iter->first);
-            count++;
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) {
+            return false;
         }
-        iter++;
     }
-    if (count == 1) {
-        for (auto x:out)
-            cout << x << " " << k;
-    } else {
-        sort(out.begin(), out.end());
-        cout << out[0] << " " << k << " " << count;
+    return true;
+}
+int hash2[10010];
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+    int arr[n];
+    while (!fun(m)) {
+        m++;
     }
+//    cout << m;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+//        cout<<arr[i]%m;
+        if (hash2[arr[i] % m]==0) {
+            hash2[arr[i] % m] = arr[i];
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (hash2[arr[i] % m] && hash2[arr[i] % m] == arr[i]) {
+            cout << arr[i] % m;
+        } else {
+            cout << "-";
+        }
+        if (i != n - 1) {
+            cout << " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
-
 /*
-4
-13005711862 13588625832
-13505711862 13088625832
-13588625832 18087925832
-15005713862 13588625832
+4 4
+10 6 4 15
 
 */
