be5293e4afef89c691c9db7582f8a35bd5fce4b9
陈明瀚 <58127395+Bhind1003@users.noreply.github.com>
2021-03-27 21:22:52 +0800

    pat train1-10 get 10points

--- ctest/time.cpp
+++ ctest/time.cpp
@@ -1,41 +1,26 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-typedef struct node {
-    int begin;
-    int end;
-} node;
-
-bool cmp(node a, node b) {
-    return a.end < b.end;
-}
 
 int main() {
     int n;
     cin >> n;
-    node a[n];
+    int a[n];
     for (int i = 0; i < n; ++i) {
-        cin >> a[i].begin >> a[i].end;
+        cin >> a[i];
     }
-    sort(a, a + n, cmp);
-//    for (int i = 0; i < n; ++i) {
-//        cout << a[i].begin << " " << a[i].end << endl;
-//    }
-    int cnt = 1,k=0;
-    for (int i = 1; i < n ; ++i) {
-        if (a[k].end < a[i].begin) {
-            k=i;
-            cnt++;
-        }
+    int pro = a[0], max = 0;
+    for (int i = 1; i < n; ++i) {
+        if (pro < 0)pro = 0;
+        pro += a[i];
+        if (max < pro)
+            max = pro;
     }
-    cout << cnt << endl;
+    cout << max;
     return 0;
 }
 /*
 5
-6 8
-1 5
-0 2
-2 3
-0 1
+-2 11 -4 13 -5 -2
+
 */
